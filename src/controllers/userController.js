86838aea1545a54774fa5556f31545936ac86b89
onrioni <onlyoni20@naver.com>
2022-07-04 01:48:14 +0900

    #5

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,5 @@
+
+
 export const join = (req, res) => res.send("join");
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
--- src/server.js
+++ src/server.js
@@ -5,11 +5,16 @@ import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
+
+
 const app = express();
 const logger = morgan("dev");
+
+
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-
-
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
